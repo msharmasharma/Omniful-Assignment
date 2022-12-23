213229137e715e1140e2a7848f49e5787588aebe
Madhusudan Sharma <msharma0077865555@gmail.com>
2022-12-24 01:33:41 +0530

    grid boxes

--- figma-design/src/pages/Saleschannel.js
+++ figma-design/src/pages/Saleschannel.js
@@ -28,14 +28,102 @@ export const Saleschannel = () => {
         <div className="channeleight">Sales Channels (8)</div>
       </div>
       <div className="allgrids">
-        <div className="boxes">1</div>
-        <div className="boxes">2</div>
-        <div className="boxes">3</div>
-        <div className="boxes">4</div>
-        <div className="boxes">5</div>
-        <div className="boxes">6</div>
-        <div className="boxes">7</div>
-        <div className="boxes">8</div>
+        <div className="boxes">
+          <div className="active">Active</div>
+          <div>
+            <img
+              className="shopify"
+              src="https://s3-alpha-sig.figma.com/img/ff89/4bca/e1956deef2865318135ffbdae64c89cb?Expires=1672617600&Signature=UKSIBz2uZocBMWsWnlqiJMFRIj~bOD67qg5G0bJmV7~jELtQ11YByo7TsShMpHSR46yccQaXl-kLkMt~mBnmcp89TfhnDvX4MMhWPv1AyNTAvI9VpFGOCqQwxyFChwjLYZU~PM1VLe~fzU1ITcuv4GXW0jcFEZQzmfLg7jdDliIKzLKUs1A~YM5ANiwumG-esNDth91H6v505V-NiYsOiN5CGV8oeGwpfYweCS69dOOv~nXjpI1CalrMrOXYhtSimgbhbQgiLjSWngjMhrRFsMuFkaXkWs6FjDxceJpYw5MD8pUrAVb2ZLDRy4ThPvuFVa279MBCPEpecaK8BzaV-A__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4"
+              alt=""
+            />
+          </div>
+          <div className="custom">Custome Name</div>
+          <div className="bottom">Shopify</div>
+        </div>
+        <div className="boxes">
+          <div className="active">Active</div>
+          <div>
+            <img
+              className="dakan"
+              src="https://s3-alpha-sig.figma.com/img/b70d/e4e6/4d434bb69d4db88f5a800f18da6ca117?Expires=1672617600&Signature=JsfZ3yBPTbX-dk5u9sNcsL06OEVsYlsvPwrH~utv1ZKmmkXb4VYmOAd-35jWJYHoRaGm1cWpJZIGfESGzaorm5JI14rzAYuDEHgiHUr7rC31CZCna76wRFLk7XrTgis7eN9i99eoJldOoDep5hGYHhzUeq6n0TkkdhMcY~t6leM7vEE7gwYo8-5Ic8ySs8akXi2AxdDkmJe3hdT3OTCaJyTku49pRBFJIEiUFBd6KnV~Vp~STH2WFcBw9VmPie4wcStoRg4qzVi2QbL7YI3oe6B7s7Fbwlp3xKJN2mtZQApb1SJz8p6b1qxdny3Jdf1~3QmgO33gB2xRSdzTbqAFbA__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4"
+              alt=""
+            />
+          </div>
+          <div className="custom">Custome Name</div>
+          <div className="bottom">Dakan</div>
+        </div>
+        <div className="boxes">
+          <div className="active">Active</div>
+          <div>
+            <img
+              className="zid"
+              src="https://s3-alpha-sig.figma.com/img/a207/0b18/34f0baf2ccd913474bbc6dab15f6db9b?Expires=1672617600&Signature=qoAhwGsGjzAC8FBEYJSBGsGPcR9fVZJ36bUSiKLxNtvq5HQblXmkD9OcxVUD8IsKo4SKmF0-kOwk1OWbiIMNSfB~dCgQ621q1s8qQhU46UJCDpfahsnmjupPvmsEQHEHCrZDLw46-zhdTwe0M-VC3caDbXvlCeZqiUHl-sem90yNoQ~vd3pJqPfplMiMmjKuuR2CkkEny0drJaVHTXEnm5ZzySPQvzigHCCkXKo1iMR-79~IYgqjDkQ8QDtUjPPhUc9bsjq3d9iE5zly94n1vDDcBbqm7HUSud6Tvw7lY7u62U-IuV4oX5OsXj~xYfn2Jyen2Mgi~RFzkkLml4r37Q__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4"
+              alt=""
+            />
+          </div>
+          <div className="custom">Custome Name</div>
+          <div className="bottom"> Zid</div>
+        </div>
+        <div className="boxes">
+          <div className="active">Active</div>
+          <div>
+            <img
+              className="panda"
+              src="https://s3-alpha-sig.figma.com/img/dca9/3590/338a3917199cbdda4204c9d145c7d8fe?Expires=1672617600&Signature=XAYCzrZIKUl1jUyB~mKbbnZUE2RLYrMkQc710NCMTsYg11E6-S4tHTlaPbHF2lX3rxL7Io7FGBt9NvLp4OJlPrlo8IF~y0gQ7uvkIE~ApuJafXMimFVblTZm~k-TQootxqsSb-4aLOuLIbddt7bBum6zyh-F7Y10M2z0ZplB~xpFAxZNrhhbO0gwsnQdvHsaITP4eUlfopKC7n8lrX4g5ogQpUYtGsh7QelhUh2PbAOqZeihSZAL1YOzd9PH9Vl~QvFN3q3icgq4i5O2C2cvBnpndL7COfScRskV4MnjHJl5vdXzUnS2A~CB2lmWB~cexGDNvQWtKNT4FuqgHlMWpQ__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4"
+              alt=""
+            />
+          </div>
+          <div className="custom">Custome Name</div>
+          <div className="bottom">Panda</div>
+        </div>
+        <div className="boxes">
+          <div className="active">Active</div>
+          <div>
+            <img
+              className="woo"
+              src="https://s3-alpha-sig.figma.com/img/31e8/389b/8edf8e8d7e465392d43949fc1316a2c3?Expires=1672617600&Signature=o7UQZDrwlO71DRuOkx1yvSU-sywb5RihZZNxecrwS6hiJBZcGSGmM46POT~3F2e3iKDNE3IdCnDkCHZap80r-4q1-LD-poYv94lBx4JHZxS2xjzyllAFZqYNELaaAytMQQtYXTRd-eB7EZrR7HmHJ48H-MnKjXQatNokZGXXdHjzJZoA4c9QyDcwbZdRNkoarMTxRkl4tgQn4rQfO1ppXQaz~moySDixKJLU4AT-WaZGxZNa~4GTTW5zr7t9592fcqe~XJ8wzGKd8VxfffMUPyN2q7xq40j3koOp4g7ScQlPzNlHOf1TYxoF~BE8zR8UvW2TIj6J7WuIpz235qiiZw__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4"
+              alt=""
+            />
+          </div>
+          <div className="custom">Custome Name</div>
+          <div className="bottom">woocommerce</div>
+        </div>
+        <div className="boxes">
+          <div className="Inactive">Inactive</div>
+          <div>
+            <img
+              className="magneta"
+              src="https://s3-alpha-sig.figma.com/img/dcf4/978d/2480886d577972ad3ba8d451e46d70c0?Expires=1672617600&Signature=b9L9laAMXXiYYVd69rF4hAf8eIwSnes7qyOuJow4XCfdUcDO1YJ0IMUelr9XO8uk78v9PsVjVCXU52w8IJtWyXwcZ8H4k1xeMe5zftmMCmY91vxdWZ4GHDhZGmSb~3oUyiaO7wg25sRlsXERKPrS8lhGJE-FcXcAtEipnt1W3dRLVyE-dcyTEhAJnl8vveKoxBJI7zdjA-hSgAXtFHgKW3ybiXbZAdjv79IbtVA4gj4mwA8utqIaZkLMbXCl1G3w0ssTmSBEuuRN5qb2VGj7nFDyhI86iiYHo3f6HIP27xbMio5WufnhT4I~yWtylZYCsGinY9~iFkb9o0FPkH4gGA__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4"
+              alt=""
+            />
+          </div>
+          <div className="custom">Custome Name</div>
+          <div className="bottom">Magneta</div>
+        </div>
+        <div className="boxes">
+          <div className="active">Active</div>
+          <div>
+            <img
+              className="opencart"
+              src="https://s3-alpha-sig.figma.com/img/738e/b533/e1456d01d0f6fc46b288e0336de4b318?Expires=1672617600&Signature=MsawlMrHHefVrr-ToC5T~gK3FXsyhbbdORGaywfHoGe44RWRFq8G5Akbnb28Brgn5VE2VqyNrlZ2Q9Uhw~G7QzqaVB171P7143z4Gf7qXxFdHIriE68QTCRzkx8N~gSnMXKu~ob4~3F4szqf74jKzC0VQxmjr~CIwBrGLkNOJLaup5KoeJTmuumImgpf~7PheiHRn5l9iP02OKV3XVldqMbuncVkfHoreRAtD5WII2z4NLVyIDhoZTgv~gdehmPB1-aXEFm9g-PT2dGT4ibGDszO94jbpMfu9zyExrNinoaQhE8834jMe-ZLYlB3D6yPrKsv1jAIhHv12bCm6ktnxA__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4"
+              alt=""
+            />
+          </div>
+          <div className="custom">Custome Name</div>
+          <div className="bottom">Opencart</div>
+        </div>
+        <div className="boxes">
+          <div className="active">Active</div>
+          <div>
+            <img
+              className="salla"
+              src="https://s3-alpha-sig.figma.com/img/91fb/81fc/d4ae906ec6ba45aa6dbd0a60978022e6?Expires=1672617600&Signature=StbT9gxSz6UWx6332NP7mDDq9Iik21NNzudrI~MGmnbzqOv2M8qu9myqe9X3gvzc9bt1KlkRsfS4banuRihnI0C4VbTnYtIJbn-jzgrfmiYUs0Mq-HbipHeM0F9FcOGg5Wi-TWLOn3QYiC5dxJBwC1lZ~S3q-ZFDbVwiqk9FHOD69Zbcp6AQTsCS4lsVLYEhlNFzoowACyOr1gEhohaL70mFvXimhVsk~T2mvleLmDywnkBstDsFILfYjIA~s7b8z4B0HuxuOuZdeZ7onLpgRjm3EwMPWPmTilWs9usbCiX7HLnYvmhS1M6TWlUylkmm4mYvSFaKmvtrJzACmt7rvQ__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4"
+              alt=""
+            />
+          </div>
+          <div className="custom">Custome Name</div>
+          <div className="bottom"> Salla</div>
+        </div>
       </div>
     </div>
   );
